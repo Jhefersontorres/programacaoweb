4cecd07bef18733d039fa3ccbfabac1d4a93ac18
Jhefersontorres <jheferson_torres@hotmail.com>
2020-07-04 16:37:46 -0400

    finalização atividade

--- Aula2606/js/index.js
+++ Aula2606/js/index.js
@@ -1,11 +1,17 @@
 $('#btnEntrar').click(
     function(){
-        if(document.getElementById("InputEmail").value === ''){
-            document.getElementById("InputEmail").innerHTML = 'Informe seu email'
-        }else{
+        document.getElementById("messagem").innerHTML ='';
 
-        }
+        var validacaoImput = ((document.getElementById("InputEmail").value != '') &&
+             (document.getElementById("InputPassword").value !='')) ? alert('dados ok') : 
+             document.getElementById("messagem").innerHTML = '<b style="color:red">Informe seu email</br>';
 
-        document.getElementById("InputEmail").value
+        login = {
+            'email':document.getElementById("InputEmail").value,
+            'senha':document.getElementById("InputPassword").value
+        };
+      
+        var requesAjax = ((login.email == 'jheferson@email.com') && (login.senha == '1234')) ? alert('Acesso permitido Ola Jheferson') : 
+                          document.getElementById("messagem").innerHTML = '<b style="color:red">Usuario e Senha invalido</br>';
     }
 )
